05e8ad76082ffa77b5eec4338be3effe687a4ff9
logonrmlocal <logonrmlocal@PA602MICRO24.fiap.com.br>
2024-04-22 11:32:14 -0300

    Procedure criada

--- Aula_Procedure.sql
+++ Aula_Procedure.sql
@@ -16,3 +16,28 @@ END;
 
 
 select * from produto2;
+
+
+CREATE OR REPLACE PROCEDURE SP_INSERT_PRODUTO (
+    v_nome produto2.nome%TYPE,
+    v_tipo produto2.tipo%TYPE,
+    v_marca produto2.marca%TYPE)
+    AS
+BEGIN
+    INSERT INTO produto2(nome, tipo, marca)
+    VALUES (v_nome, v_tipo, v_marca);
+END SP_INSERT_PRODUTO;
+
+CALL  SP_INSERT_PRODUTO('TENIS', 'CALÇADO', 'NIKE');
+
+
+
+CREATE OR REPLACE PROCEDURE SP_DELETE_PRODUTO (
+    P_ID produto2.ID%TYPE)
+    AS
+BEGIN
+    DELETE FROM PRODUTO2 WHERE ID = P_ID;
+    COMMIT;
+END SP_DELETE_PRODUTO;
+
+
